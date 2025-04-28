31d9c776f0edb7f0ed5a196a5c4593a7872c43a4
muriloorias <156871325+muriloorias@users.noreply.github.com>
2025-04-28 11:07:27 +0000

    codigo do vichiato

--- js/navigarion.js
+++ js/navigarion.js
@@ -2,5 +2,33 @@ function paleolitico(){
       window.location.href = 'pagina1.html';
 }
 function returnToHome(){
-      window.location.href = 'home.html'
+      window.location.href = 'home.html';
+}
+function neolitico(){
+      window.location.href = 'pagina2.html';
+}
+ 
+function antiguidade(){
+      window.location.href = 'pagina3.html';
+
+}
+
+function idademedia(){
+      window.location.href = 'pagina4.html';
+
+}
+
+function revolucaoindustrial(){
+      window.location.href = 'pagina5.html'
+
+}
+
+function seculoxx(){
+      window.location.href = 'pagina6.html'
+
+}
+
+function seculoxxi(){
+window.location.href = 'pagina7'
+
 }
