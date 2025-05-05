d52f01d0631b7f4cc2ceba5c9ec25dfeb4edc705
Vic1607 <victorvichiatonogueira@gmail.com>
2025-05-05 11:07:05 +0000

    adicionando paginas

--- js/navigarion.js
+++ js/navigarion.js
@@ -1,34 +1,42 @@
-function paleolitico(){
+//funções para navegação funcionaram iguais e devem manter o mesmo padrão
+//nome da função
+function paleolitico() {
+      //parte principal que faz a movimentação das paginas
       window.location.href = 'pagina1.html';
 }
-function returnToHome(){
+function returnToHome() {
       window.location.href = 'home.html';
 }
-function neolitico(){
+function neolitico() {
       window.location.href = 'pagina2.html';
 }
- 
-function antiguidade(){
+
+function antiguidade() {
       window.location.href = 'pagina3.html';
 
 }
 
-function idademedia(){
+function idademedia() {
       window.location.href = 'pagina4.html';
 
 }
 
-function revolucaoindustrial(){
+function revolucaoindustrial() {
       window.location.href = 'pagina5.html'
 
 }
 
-function seculoxx(){
+function seculoxx() {
       window.location.href = 'pagina6.html'
 
 }
 
-function seculoxxi(){
-window.location.href = 'pagina7'
+function seculoxxi() {
+      window.location.href = 'pagina7.html'
+
+}
+
+function curiopag1(){
+      window.location.href = 'curiosidadepag1.html'
 
 }
